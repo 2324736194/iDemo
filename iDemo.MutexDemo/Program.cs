c4a589a0ace4d159a8681466d9f3810e16468706
黄钊 <huangzhao01@whty.com.cn>
2022-01-10 11:10:20 +0800

    添加项目文件。

---
+++ iDemo.MutexDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace iDemo.MutexDemo
+{
+    public static class Program
+    {
+        private static readonly Mutex mutex;
+
+        static Program()
+        {
+            var ownerType = typeof(Program);
+            mutex = new Mutex(true, ownerType.Name);
+        }
+
+        [STAThread]
+        private static int Main(string[] args)
+        {
+            var result = 0;
+            var signal = mutex.WaitOne(0);
+            if (!signal)
+            {
+                //MessageBox.Show("应用程序已启动");
+                return result;
+            }
+            var app = new App();
+            app.InitializeComponent();
+            result = app.Run();
+            return result;
+        }
+    }
+}
